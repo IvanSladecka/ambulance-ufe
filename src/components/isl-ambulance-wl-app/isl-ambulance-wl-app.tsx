2ed452202332b855b4a81d7f5f842871c427d8db
Ivan Sladecka <ivan.sladecka@siemens-healthineers.com>
2024-01-12 14:59:00 +0100

    Predpokladaný čas vstupu

--- src/components/isl-ambulance-wl-app/isl-ambulance-wl-app.tsx
+++ src/components/isl-ambulance-wl-app/isl-ambulance-wl-app.tsx
@@ -38,6 +38,7 @@ export class IslAmbulanceWlApp {
   }
 
   render() {
+    console.debug("isl-ambulance-wl-app.render() - path: %s", this.relativePath);
     let element = "list"
     let entryId = "@new"
   
